0fb6372ec365ae517d125d8f7dd365ffc8745c9e
Atiqur Rahman <atiqur.india@gmail.com>
2020-12-14 22:34:33 +0530

    Send text to next screen

--- app/src/main/java/com/example/showactivity/MainActivity.java
+++ app/src/main/java/com/example/showactivity/MainActivity.java
@@ -2,17 +2,41 @@ package com.example.showactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button nextScreenButton;
     private EditText inputTextBox;
+    private final int REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextScreenButton = findViewById(R.id.button_next_screen);
+        inputTextBox = findViewById(R.id.editTextTextPersonName);
+
+        nextScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = inputTextBox.getText().toString().trim();
+
+                if (!inputText.isEmpty()) {
+                    Intent intent = new Intent(MainActivity.this, ShowText.class);
+                    intent.putExtra("inputText", inputText);
+                    intent.putExtra("name", "Bond");
+                    intent.putExtra("age", 35);
+                    startActivityForResult(intent, REQUEST_CODE);
+                } else {
+                    Toast.makeText(MainActivity.this, "Enter text", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/showactivity/ShowText.java
+++ app/src/main/java/com/example/showactivity/ShowText.java
@@ -2,16 +2,30 @@ package com.example.showactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ShowText extends AppCompatActivity {
-
+    private TextView showText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_text);
+
+        Bundle extra = getIntent().getExtras();
+
+        showText = findViewById(R.id.textView);
+
+        if (extra != null) {
+            showText.setText(extra.getString("inputText"));
+            Log.d("NAME", "onCreate: " + extra.getString("name"));
+            Log.d("AGE", "onCreate: " + extra.getInt("age"));
+        }
     }
 }
